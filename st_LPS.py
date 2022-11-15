b7dbae279870a2e929d6a514c59df2813c46e445
LEVRAUD <laurent.levraud@gmail.com>
2022-11-15 13:31:36 +0100

    change page to wide

--- st_LPS.py
+++ st_LPS.py
@@ -1,12 +1,12 @@
 #C:\\Users\\wonde\\Documents\\Anouk\\LPS\\Image\\jpg\\rmbg\\github\\LPS
+#manuscript est utiliser pour multirépertoires
 import streamlit as st
 import glob,os
 
 
 def load_images():
-   # image_files = glob.glob("./*/*.jpg")
+   # image_files = glob.glob("./*/*.jpg")  #multirepertories
     image_files = glob.glob("./resizes/*.jpg")
-  #  st.write(image_files)
     name_files=[]
 
     for image_file in image_files:
@@ -20,7 +20,7 @@ def load_images():
     #st.write(name_files)
         
     return image_files, name_files#, names
-
+st.set_page_config(layout="wide")
 st.title("Mes Pets Shop")
 st.write("""### Tableau de mes LPS """)
 n = st.number_input("Sélectionne le nombre de colonne",1,5,4)
@@ -34,14 +34,8 @@ for image_file in image_files:
         view_images.append(image_file)
         
 groups=[]
-#names_group=[]
 for i in range(0,len(view_images),n):
     groups.append(view_images[i:i+n])
-
-#for i in range(0,len(name_files),n): 
-#   names_group.append(name_files[i:i+n])
-                 
-#st.write(groups) #pour controler la création de groupe
 
 for group in groups:
     cols = st.columns(n)
@@ -49,6 +43,5 @@ for group in groups:
         names_files = os.path.splitext(image_file)[0]
         names_files=names_files.split('\\')[-1]
         names_files=names_files.split('_')[-1]
-        
         cols[i].image(image_file, caption=str(names_files))
-#st.write(i,image_files[i])
+
